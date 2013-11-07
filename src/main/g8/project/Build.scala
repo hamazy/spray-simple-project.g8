a4ff8b0de940ebdda453cf0a1b6904a4fbb0fcbb
Suguru Hamazaki <hamazy@webspiritus.com>
2013-11-07 13:25:20 +0900

    * Parameterize the version string * Use sbt-assembly, scct, stalastyle * Update scalatest

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -4,21 +4,23 @@ import Keys._
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "$package$",
-    version := "1.0-SNAPSHOT",
+    version := "$version$",
     scalaVersion := "2.10.2",
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-encoding", "utf8")
   )
 }
 
 object Dependencies {
-  val sprayCan = "io.spray"            %   "spray-can"         % "1.2-M8"
-  val sprayRouting = "io.spray"            %   "spray-routing"     % "1.2-M8"
-  val sprayTest = "io.spray"            %   "spray-testkit"     % "1.2-M8" % "test"
-  val akkaActor = "com.typesafe.akka"   %%  "akka-actor"        % "2.2.0-RC1"
-  val akkaTestkit = "com.typesafe.akka"   %%  "akka-testkit"      % "2.2.0-RC1" % "test"
-  val scalaLogging = "com.typesafe"        %% "scalalogging-slf4j" % "1.0.1"
-  val scalatest = "org.scalatest"       %% "scalatest"          % "1.9.1" % "test"
-  val mockito = "org.mockito"         % "mockito-core"        % "1.9.5" % "test"
+  val sprayCan = "io.spray" % "spray-can" % "1.2-M8"
+  val sprayRouting = "io.spray" % "spray-routing" % "1.2-M8"
+  val sprayTest = "io.spray" % "spray-testkit" % "1.2-M8" % "test"
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.0-RC1"
+  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.2.0-RC1" % "test"
+  val scalaLogging = "com.typesafe" %% "scalalogging-slf4j" % "1.0.1"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.0.13"
+  val scalatest = "org.scalatest" %% "scalatest" % "2.0.M8" % "test"
+  val pegdown = "org.pegdown" % "pegdown" % "1.0.1" % "test"
+  val mockito = "org.mockito" % "mockito-core" % "1.9.5" % "test"
 
 val commonDeps = Seq(
     sprayCan,
@@ -28,6 +30,7 @@ val commonDeps = Seq(
     akkaTestkit,
     scalaLogging,
     scalatest,
+    pegdown,
     mockito
   )
 }
@@ -43,10 +46,20 @@ object $name;format="Camel"$Build extends Build {
   import BuildSettings.buildSettings
   import Dependencies.commonDeps
   import spray.revolver.RevolverPlugin.Revolver
+  import org.scalastyle.sbt.ScalastylePlugin.{Settings => ScalastyleSettings}
+  import sbtassembly.Plugin.AssemblyKeys
+  import sbtassembly.Plugin.assemblySettings
+  import ScctPlugin.instrumentSettings
 
-  lazy val root = Project("$name;format="normalize"$", 
-			  file("."),
-			  settings = buildSettings ++
-				Seq(libraryDependencies ++= commonDeps,
-				    resolvers := Resolvers.resolvers) ++ Revolver.settings)
+  lazy val root = Project("$name;format="normalize"$",
+    file("."),
+    settings = buildSettings ++
+      instrumentSettings ++
+      ScalastyleSettings ++
+      assemblySettings ++
+      Revolver.settings) settings (
+    libraryDependencies ++= commonDeps,
+        resolvers := Resolvers.resolvers,
+        mainClass in AssemblyKeys.assembly := Some("$organization$.Boot"),
+        testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/report", "-o"))
 }
