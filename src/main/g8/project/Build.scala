9c338e6c68409cb7da25d0f12f63bd94d0b9c5dd
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-04-14 18:25:00 +0900

    Update Spray and Scala version, and add spray-json.

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -11,11 +11,11 @@ object BuildSettings {
 }
 
 object Dependencies {
-  val akkaVersion = "2.2.3"
-  val sprayVersion = "1.2.0"
+  val akkaVersion = "2.3.0"
+  val sprayVersion = "1.3.1"
   val sprayCan = "io.spray" % "spray-can" % sprayVersion
   val sprayRouting = "io.spray" % "spray-routing" % sprayVersion
-  val sprayJson = "io.spray" %% "spray-json" % "1.2.5"
+  val sprayJson = "io.spray" %% "spray-json" % "1.2.6"
   val sprayTest = "io.spray" % "spray-testkit" % sprayVersion % "test"
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
   val akkaSlf4j = "com.typesafe.akka"   %%  "akka-slf4j" % akkaVersion
