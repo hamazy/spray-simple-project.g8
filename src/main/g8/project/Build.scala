12e80c30c87aa1d0b4d601eafc5326998738cde1
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-08-30 21:38:45 +0900

    Update Scala, Akka, and other libraries.

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -5,26 +5,26 @@ object BuildSettings {
   val buildSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "$package$",
     version := "$version$",
-    scalaVersion := "2.10.3",
+    scalaVersion := "2.11.1",
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-encoding", "utf8"),
     shellPrompt := { state => Project.extract(state).currentProject.id + " > " }
   )
 }
 
 object Dependencies {
-  val akkaVersion = "2.3.0"
+  val akkaVersion = "2.3.2"
   val sprayVersion = "1.3.1"
-  val sprayCan = "io.spray" % "spray-can" % sprayVersion
-  val sprayRouting = "io.spray" % "spray-routing" % sprayVersion
+  val sprayCan = "io.spray" %% "spray-can" % sprayVersion
+  val sprayRouting = "io.spray" %% "spray-routing" % sprayVersion
   val sprayJson = "io.spray" %% "spray-json" % "1.2.6"
-  val sprayTest = "io.spray" % "spray-testkit" % sprayVersion % "test"
+  val sprayTest = "io.spray" %% "spray-testkit" % sprayVersion % "test"
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
   val akkaSlf4j = "com.typesafe.akka"   %%  "akka-slf4j" % akkaVersion
   val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test"
-  val scalaLogging = "com.typesafe" %% "scalalogging-slf4j" % "1.0.1"
-  val logback = "ch.qos.logback" % "logback-classic" % "1.0.13"
-  val scalatest = "org.scalatest" %% "scalatest" % "2.1.5" % "test"
-  val pegdown = "org.pegdown" % "pegdown" % "1.0.1" % "test"
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "2.1.2"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.1.2"
+  val scalatest = "org.scalatest" %% "scalatest" % "2.2.0" % "test"
+  val pegdown = "org.pegdown" % "pegdown" % "1.4.2" % "test"
   val mockito = "org.mockito" % "mockito-core" % "1.9.5" % "test"
 
   val commonDeps = Seq(
@@ -57,7 +57,7 @@ object $name;format="Camel"$Build extends Build {
   import org.scalastyle.sbt.ScalastylePlugin.{Settings => ScalastyleSettings}
   import sbtassembly.Plugin.AssemblyKeys
   import sbtassembly.Plugin.assemblySettings
-  import ScctPlugin.instrumentSettings
+  import scoverage.ScoverageSbtPlugin.instrumentSettings
 
   lazy val root = Project("$name;format="normalize"$",
     file("."),
--- src/main/g8/project/plugins.sbt
+++ src/main/g8/project/plugins.sbt
@@ -2,8 +2,6 @@ addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.1")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.3.2")
 
-addSbtPlugin("com.github.scct" % "sbt-scct" % "0.2.1")
+addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "0.99.7.1")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.2")
-
-resolvers += "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases/"
